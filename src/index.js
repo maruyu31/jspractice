0d58e54968d53e31d2a349908825d82a1f007799
YuMaru <72259047+maruyu31@users.noreply.github.com>
2021-06-26 10:34:03 +0900

    finish js-practice

--- src/index.js
+++ src/index.js
@@ -97,7 +97,7 @@
 // sayHello("maru");
 
 /**
- * スプレット構文 ...
+ * スプレット構文 //...
  */
 // 配列の展開
 // const arr1 = [1, 2];
@@ -124,3 +124,76 @@
 
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+// const nameArr = ["maru", "yama", "yuu"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
+
+// const nameArr2 =nameArr.map((name) => {
+//   return name;
+// })
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`))
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num %2 === 0;
+// })
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "yuu") {
+//     return name;
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? 'true' : 'false'
+// console.log(val1)
+
+// const num = "1300";
+// console.log(num.toLocaleString(num));
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください'
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲ないです';
+// }
+
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう　 $$ ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// };
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// };
+
+//  ||は左側がfalseとなるときに右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
